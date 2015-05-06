93fd780466a4ed54d236f635a63eb6d2c6b30238
lincharlie8 <lincharlie1@yahoo.co.jp>
2015-05-06 12:00:54 +0800

    test php 上傳

---
+++ test.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+</head>
+<body>
+	<?php
+		$link =mysql_connect("localhost","mobileUser","ntustmobile") or die("Wrong!");
+		echo "Sucesses!!";
+	?>
+
+</body>
+</html>
