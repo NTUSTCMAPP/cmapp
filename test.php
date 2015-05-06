0f674ac88e9d2426c599f55c88567bb28c6eba95
lincharlie8 <lincharlie1@yahoo.co.jp>
2015-05-06 13:02:48 +0800

    sql_connect

--- test.php
+++ test.php
@@ -8,10 +8,10 @@
 	<?php
 
 		$DB="cmapp";
-		$con=mssql_connect("localhost","mobileUser","ntustmobile") or die("Wrong!");
-		//$link =mysql_connect
+		//$con=mssql_connect("localhost","mobileUser","ntustmobile") or die("Wrong!");
+		$link =mysql_connect("localhost","mobileUser","ntustmobile") or die("Wrong!");
 		echo "Sucesses!!";
-		/*$sql="SELECT * FROM USER";
+		$sql="SELECT * FROM USER";
 		$result=execute_sql($DB,$sql,$link);
 
 		while($row =mysql_fetch_array($result,MYSQL_BOTH)){
@@ -19,7 +19,7 @@
 			echo $row["UserNAme"]."<br>";
 
 		}
-		mysql_free_result($result);*/
+		mysql_free_result($result);
 		mysql_close($link);
 	?>
 
