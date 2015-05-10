22e270288e351b4629cec54b73ce80e688baa69f
s9351008 <s9351008@gmail.com>
2015-05-10 23:21:59 +0800

    網路上的php測試

---
+++ AndroidConnectingToPhpMySQL/android_connect/db_config.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+ * All database connection variables
+ */
+
+define('DB_USER', "mobileDesigner"); // db user
+define('DB_PASSWORD', "ntustmobile"); // db password (mention your db password here)
+define('DB_DATABASE', "androidhive"); // database name
+define('DB_SERVER', "cmapp.nado.tw"); // db server
+?>
