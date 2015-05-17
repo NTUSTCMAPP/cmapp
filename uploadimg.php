2f68834778d252024cd25712404196f6715ee19f
lincharlie8 <lincharlie1@yahoo.co.jp>
2015-05-17 12:21:50 +0800

    UPLOAD PHOTO string

--- uploadimg.php
+++ uploadimg.php
@@ -22,7 +22,7 @@ if(is_dir("./photo/")){
 	    }else{
 	    	chmod($target_path, 0777);
 	    }
-	    $website = isset($_POST['website']) ? $_POST['website'] : '';
+	    $website = isset($_POST['website']) ? $_POST['website'] : 'no';
 	    
 
 	    echo "The file " . basename($_FILES['image']['name']) .
