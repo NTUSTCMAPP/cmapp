5065c565eeb61f27fec180a4771f06ba95346629
lincharlie8 <lincharlie1@yahoo.co.jp>
2015-05-17 12:36:21 +0800

    fileName

--- uploadimg.php
+++ uploadimg.php
@@ -7,10 +7,30 @@ echo $_FILES['image']['name'] . '<br/>';
 //ini_set('post_max_size', '10M');
 //ini_set('max_input_time', 300);
 //ini_set('max_execution_time', 300);
+require_once __DIR__ . '/android_connect_user/db_connect.php';
 if(is_dir("./photo/")){
+	$db = new DB_CONNECT();
+	$UserIndex = basename($_FILES['image']['name']);
+	$fileName="";
 	$target_path = "./photo/";
 
-	$target_path = $target_path . basename($_FILES['image']['name']);
+	$result = mysql_query("SELECT UserID FROM User WHERE UserIndex = $UserIndex");
+	if (mysql_num_rows($result) > 0) {
+
+    
+  	  	$row = mysql_fetch_array($result);
+		$fileName =$row["UserID"];
+		$target_path = $target_path . $fileName.".jpg";
+
+    }else{
+
+		$target_path = $target_path . basename($_FILES['image']['name']).".jpg";
+		$fileName= basename($_FILES['image']['name']);
+    }
+        //String to Integer
+	
+
+	
 	//$target_path = $target_path . "test.jpg";
 
 
@@ -22,11 +42,10 @@ if(is_dir("./photo/")){
 	    }else{
 	    	chmod($target_path, 0777);
 	    }
-	    $website = basename($_FILES['website']['name']);
-	    
+	   
 
-	    echo "The file " . basename($_FILES['image']['name']) .
-	    " has been uploaded //webSite:".$website;
+	    echo "The file " . $fileName .
+	    " has been uploaded"
 	} catch (Exception $e) {
 	    die('File did not upload: ' . $e->getMessage());
 	}
